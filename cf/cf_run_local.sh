8a36dfa928b3fc53d4f98de55b569d9cce36c0fd
Aliaksandr Karmyzau <aliaksandr.karmyzau@leverx.com>
2021-10-11 15:34:01 +0300

    Commented services in manifest

--- cf/cf_run_local.sh
+++ cf/cf_run_local.sh
@@ -7,4 +7,4 @@ export AMQP_URI=amqp://guest:guest@3.64.162.116:5672
 export POSTGRES_URI=postgresql://postgres:password@3.64.162.116:5432
 export REDIS_URI=redis://3.64.162.116:6379
 
-./run.sh $@ 
+../run.sh $@ 
